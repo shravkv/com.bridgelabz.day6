dbbdfc8b1d94a3922dc53b23754b4a601db8be2e
sravan <sravan.k.vodnala@gmail.com>
2022-05-28 17:02:06 +0530

    Added

---
+++ src/com/bridgelabs/day6/Fibonacci.java
@@ -0,0 +1,20 @@
+package com.bridgelabs.day6;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int n = 10 , firstTerm = 0 , secondTerm = 1;
+        System.out.println("Fibonacci Series till " + n + " terms:");
+
+        for (int i =1; i <= n; ++i) {
+            System.out.println(firstTerm + ",");
+
+            // compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+
+        }
+    }
+}
