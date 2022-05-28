0e7a9fb0e804b439e11d3d42f53e491bd7f9dec5
sravan <sravan.k.vodnala@gmail.com>
2022-05-28 17:40:06 +0530

    reverse

---
+++ src/primenum/Prime.java
@@ -0,0 +1,21 @@
+package primenum;
+
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int num = 23;
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i ) {
+            // condition for non-prime number
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println(num + " is a prime number .");
+        else
+            System.out.println(num + " is not a prime number .");
+    }
+}
---
+++ src/reversenum/Reverse.java
@@ -0,0 +1,16 @@
+package reversenum;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+
+        int num = 2356, reversed = 0;
+
+        for(;num != 0; num /= 10) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
